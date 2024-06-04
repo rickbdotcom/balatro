b86c5d0360ef4179f442e76ad8c38acfdd713ccb
Richard Burgess <rickb@rickb.com>
2024-06-04 18:09:52 -0500

    backup

--- Sources/BalatroTool/Backup.swift
+++ Sources/BalatroTool/Backup.swift
@@ -20,21 +20,36 @@ struct Backup: AsyncParsableCommand {
     var profile: String?
 
     func run() async throws {
-        let fs = FileSystem()
-        try fs.createBackupDirectory(for: profile)
+        let fileSystem = FileSystem()
 
-        let saveGameURL = fs.saveGameURL(for: profile)
-        let backupDirectory = fs.backupDirectory(for: profile)
+        try fileSystem.createBackupDirectory(for: profile)
+        let saveGameURL = fileSystem.saveGameURL(for: profile)
 
         let monitor = try FileMonitor(url: saveGameURL)
         for try await _ in monitor.events {
-            try backup(saveGameURL, to: backupDirectory)
+            try backup()
         }
     }
 
-    func backup(_ url: URL, to directory: URL) throws {
-        let fileName = String(Int(Date.timeIntervalSinceReferenceDate))
-        let dstURL = URL(fileURLWithPath: fileName + ".jkr", relativeTo: directory)
-        try FileManager.default.copyItem(at: url, to: dstURL)
+    func backup() throws {
+        let fileSystem = FileSystem()
+
+        let saveGameURL = fileSystem.saveGameURL(for: profile)
+        let backupDirectory = fileSystem.backupDirectory(for: profile)
+        
+        let jkrDict = try Dictionary(contentsOfJKR: saveGameURL)
+        
+        let game = jkrDict["GAME"] as? [AnyHashable: Any]
+
+        let round_scores = game?["round_scores"] as? [AnyHashable: Any]
+        let furthest_ante = round_scores?["furthest_ante"]  as? [AnyHashable: Any]
+        let ante = furthest_ante?["amt"]  as? Int ?? 0
+
+        let round = game?["round"]  as? Int ?? 0
+
+        let dateString = String(Int(Date.timeIntervalSinceReferenceDate))
+        let fileName = "\(dateString)_0\(ante)_0\(round).jkr"
+        let dstURL = URL(fileURLWithPath: fileName, relativeTo: backupDirectory)
+        try FileManager.default.copyItem(at: saveGameURL, to: dstURL)
     }
 }
